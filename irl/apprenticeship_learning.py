2156885954a759e48039296e1e2a747c6e912617
dh <dhfromkorea@gmail.com>
2018-04-04 16:40:04 -0400

    update mc estimator for mu

--- irl/apprenticeship_learning.py
+++ irl/apprenticeship_learning.py
@@ -7,19 +7,15 @@ from lstd import LSTDQ, LSTDMu, LSPI
 from policy import LinearQ2
 
 class ApprenticeshipLearning(object):
-    """Docstring for ApprenticeshipLearning. """
+    """ApprenticeshipLearning continuous state
 
-    def __init__(self):
-        """TODO: to be defined1. """
-        pass
+    assuming we have access to simulator
+    everything is now estimated through Monte Carlo
 
-
-class BatchApprenticeshipLearning(ApprenticeshipLearning):
-    """Batch ApprenticeshipLearning continuous state"""
+    """
 
     def __init__(self, env,
                        pi_init,
-                       D,
                        action_list,
                        p,
                        q,
@@ -31,6 +27,7 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
                        init_s_sampler,
                        mu_sample_size,
                        precision,
+                       mdp_solver=None,
                        use_slack=False,
                        slack_penalty=0.01
                        ):
@@ -41,7 +38,6 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
         env : TODO
         pi_init : TODO
 
-        D : trajectory data
         p : dimension of phi
         q : dimension of psi
         phi : basis function for reward
@@ -58,7 +54,6 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
         self._env = env
         self._pi_init = pi_init
 
-        self._D = D
         self._action_list = action_list
         self._p = p
         self._q = q
@@ -71,6 +66,7 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
         self._mu_sample_size = mu_sample_size
 
         self._precision = precision
+        self._mdp_solver = mdp_solver
         self._use_slack = use_slack
         self._slack_penalty = slack_penalty
 
@@ -135,7 +131,320 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
 
                 # @todo: allow non-batch solver also
                 # solve the mdpr
-                pi_irl = self.solve_mdpr(reward_fn)
+                if self._mdp_solver is None:
+                    pi_irl = self.solve_mdpr(reward_fn)
+                else:
+                    pi_irl = self._mdp_solver.solve(reward_fn)
+
+                pi_list.append(pi_irl)
+                logging.debug("pi_irl: {}".format(pi_irl._W))
+
+                # record new mu_irl
+                al = ApprenticeshipLearning
+                mu_irl = al.estimate_mu(env=env,
+                                        pi_eval=pi_irl,
+                                        mu_sample_size=self._mu_sample_size,
+                                        phi=self._phi,
+                                        gamma=self._gamma,
+                                        return_epi_len=False)
+                mu_list.append(mu_irl)
+                logging.debug("mu_irl: {}".format(mu_irl))
+
+            # save trial-level data
+            margin_v_collection.append(margin_v_list)
+            margin_mu_collection.append(margin_mu_list)
+            pi_collection.append(pi_list)
+            weight_collection.append(weight_list)
+            mu_collection.append(mu_list)
+
+
+            # choose the best policy for each trial
+            pi_best = self._choose_pi_best(mu_list, pi_list)
+            pi_best_collection.append(pi_best)
+            logging.info("pi_best: {}".format(pi_best._W))
+
+
+        # dump save the important meta data to numpy
+        results = {
+                "margin_v": margin_v_collection,
+                "margin_mu": margin_mu_collection,
+                "mu": mu_collection,
+                "weight": weight_collection,
+                "policy": pi_collection,
+                "solutions" : pi_best_collection,
+                }
+        return results
+
+
+    def _choose_pi_best(self, mu_list, pi_list):
+        """TODO: Docstring for choose_pi_best.
+
+        Parameters
+        ----------
+        pi_list : TODO
+
+        Returns
+        -------
+        pi_best
+
+        TODO
+        - try solve for best convex combination to minimize mu distance
+
+        """
+        pi_best = pi_list[np.argmin(np.abs(self._mu_exp - np.array(mu_list)))]
+        return pi_best
+
+
+    @staticmethod
+    def estimate_mu(self, env, pi_eval, mu_sample_size, phi, gamma, return_epi_len=False):
+        """TODO: Docstring for something.
+
+        need to refit using a new policy to evaluate
+        Parameters
+        ----------
+        pi_eval : policy under which to estimate mu
+
+        Returns
+        -------
+        TODO
+
+        """
+		mus = []
+        epi_length_list = []
+
+		for epi_i in range(self._mu_sample_size):
+			# initial state is not fixed
+			s = env.reset()
+			ss_init.append(s)
+			mu = 0.0
+			for t in itertools.count():
+				a = pi_eval.choose_action(s)
+				s_next, r, done, _ = env.step(a)
+				mu += gamma ** t * phi(s, a)
+				s = s_next
+				if done:
+					break
+            epi_length_list.append(t)
+			mus.append(mu)
+
+        mu_hat = np.array(mu_list).mean(axis=0)
+        if return_epi_len:
+			epi_length_avg = np.mean(epi_length_list)
+			return mu_hat, epi_length_avg
+        else:
+            return mu_hat
+
+
+    def _get_reward_fn(self, W):
+        """linearly parametrize reward function.
+
+        Parameters
+        ----------
+        W : weight
+
+        Returns
+        -------
+        TODO
+        - think whether to do s, a or just s
+
+        """
+        #return lambda s : W.dot(self.phi(s))
+        return lambda s, a : W.T.dot(self._phi(s, a).T)
+
+
+    def _optimize(self, mu_list):
+        """linearly parametrize reward function.
+
+        implements eq 11 from Abbeel
+        note: we can rewrite this as an SVM problem
+
+        Parameters
+        ----------
+        W : weight
+
+        Returns
+        -------
+        TODO
+        - think whether to do s, a or just s
+
+        """
+        logging.info("solving for W given mu_list")
+        # define variables
+        W = cvx.Variable(self._p)
+        t = cvx.Variable(1)
+
+        if self._use_slack:
+            #xi = cvx.Variable(len(mu_list))
+            xi = cvx.Variable(1)
+
+        mu_exp = cvx.Parameter(self._p)
+        mu_exp.value = self._mu_exp.flatten()
+
+        if self._use_slack:
+            C = cvx.Parameter(1)
+            C = self._slack_penalty
+            # since obj is max
+            # we should penalize xi with minus
+            # bc. xi bumps up expert's reward (see below)
+            obj = cvx.Maximize(t - C * xi)
+        else:
+            obj = cvx.Maximize(t)
+
+        constraints = []
+
+        for mu in mu_list:
+            if self._use_slack:
+                # xi helps expert to perform better
+                constraints += [W.T * mu_exp + xi >= W.T * mu + t]
+            else:
+                constraints += [W.T * mu_exp >= W.T * mu + t]
+        constraints += [cvx.norm(W, 2) <= 1]
+
+        prob = cvx.Problem(obj, constraints)
+        prob.solve()
+
+        # if svm formulation, need to normalize
+        # W = W.value / np.linalg(W.value, 2)
+
+        margin_v = t.value
+        converged = margin_v <= self._precision
+
+        # convergence in mu implies convergence in value (induced convergence)
+        # but we don't use this relation here
+        margin_mu = np.min(np.abs(np.array(mu_exp.value) - np.array(mu_list)))
+
+        return np.array(W.value), (margin_v, margin_mu, converged)
+
+
+# @todo: establish the two classes through inheritance
+class BatchApprenticeshipLearning(object):
+    """Batch ApprenticeshipLearning continuous state"""
+
+    def __init__(self, env,
+                       pi_init,
+                       D,
+                       action_list,
+                       p,
+                       q,
+                       phi,
+                       psi,
+                       gamma,
+                       eps,
+                       mu_exp,
+                       init_s_sampler,
+                       mu_sample_size,
+                       precision,
+                       mdp_solver=None,
+                       use_slack=False,
+                       slack_penalty=0.01
+                       ):
+        """TODO: to be defined1.
+
+        Parameters
+        ----------
+        env : TODO
+        pi_init : TODO
+
+        D : trajectory data
+        p : dimension of phi
+        q : dimension of psi
+        phi : basis function for reward
+        psi : basis function for feature expectation
+        gamma : discount factor (0, 1)
+        eps : small positive value to make A invertible
+        mu_exp : TODO
+        init_s_sampler: initial state sampler
+        mu_sample_size : sample size to account for varying init states
+        precision : convergence threshold
+        use_slack : whether to use slack for convex optimization
+        slack_penalty : scaling term
+        """
+        self._env = env
+        self._pi_init = pi_init
+
+        self._D = D
+        self._action_list = action_list
+        self._p = p
+        self._q = q
+        self._phi = phi
+        self._psi = psi
+        self._gamma = gamma
+        self._eps = eps
+        self._mu_exp = mu_exp
+        self._init_s_sampler = init_s_sampler
+        self._mu_sample_size = mu_sample_size
+
+        self._precision = precision
+        self._mdp_solver = mdp_solver
+        self._use_slack = use_slack
+        self._slack_penalty = slack_penalty
+
+
+    def run(self, n_trial, n_iteration):
+        """TODO: Docstring for something.
+
+        Parameters
+        ----------
+        n_trial : TODO
+        n_iteration : TODO
+        D : batch data under pi_expert
+
+        Returns
+        -------
+        TODO
+        - check if we should use MC estimator for mu_exp or lstd
+        - check if to marginalize action out from mu estimator
+        - check how to sample initial state
+        - check scaler
+        """
+        mu_exp = self._mu_exp
+
+        # todo: traj legnth or value of pi_expert
+        margin_v_collection = []
+        margin_mu_collection = []
+        pi_collection = []
+        weight_collection = []
+        mu_collection = []
+        pi_best_collection = []
+
+        for trial_i in tqdm(range(n_trial)):
+            margin_v_list = []
+            margin_mu_list = []
+            pi_list = []
+            weight_list = []
+            mu_list = []
+
+            # pick a random policy as init policy
+            mu_irl = self.estimate_mu(self._pi_init)
+            mu_list.append(mu_irl)
+            pi_list.append(self._pi_init)
+
+            # todo replace with cvxpy
+
+            for epi_i in range(n_iteration):
+                W, (margin_v, margin_mu, converged) = self._optimize(mu_list)
+
+                # record margin_v, margin_mu
+                weight_list.append(W)
+                margin_v_list.append(margin_v)
+                margin_mu_list.append(margin_mu)
+                logging.info("W: {}".format(W))
+                logging.info("margin_v: {}".format(margin_v))
+                logging.info("margin_mu: {}".format(margin_mu))
+
+                if converged:
+                    logging.info("margin_mu converged after {} iterations".format(epi_i + 1))
+                    break
+
+                reward_fn = self._get_reward_fn(W=W)
+
+                # @todo: allow non-batch solver also
+                # solve the mdpr
+                if self._mdp_solver is None:
+                    pi_irl = self.solve_mdpr(reward_fn)
+                else:
+                    pi_irl = self._mdp_solver.solve(reward_fn)
+
                 pi_list.append(pi_irl)
                 logging.debug("pi_irl: {}".format(pi_irl._W))
 
@@ -173,6 +482,8 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
     def solve_mdpr(self, reward_fn):
         """TODO: Docstring for solve_mdpr
 
+        note: not working
+
         note W here is a parameter for Q, not for reward
         assume D is good enough
         we should be able to find a good Q_hat
@@ -186,6 +497,7 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
         TODO
 
         """
+        raise Exception("need to be debugged")
         logging.info("solving MDP\R with LSPI")
         # modify reward
         # pi = phi(s,a)^T W_0
@@ -211,7 +523,6 @@ class BatchApprenticeshipLearning(ApprenticeshipLearning):
                       phi=self._phi,
                       W=W)
         return pi
-
 
 
     def _choose_pi_best(self, mu_list, pi_list):
--- run_exp.py
+++ run_exp.py
@@ -15,7 +15,9 @@ from lstd import LSTDQ, LSTDMu, LSPI
 from envs.simulator import Simulator
 from policy import *
 from utils import *
-from irl.apprenticeship_learning import BatchApprenticeshipLearning as BAL
+from irl.apprenticeship_learning import ApprenticeshipLearning as AL
+from fa import LinearQ3
+import plotting
 
 
 class NearExpertPolicy():
@@ -155,7 +157,8 @@ def main():
     use_slack = False
     # @note: reward may have to be scaled to work with slack penalty
     slack_penalty = 1e-3
-    eps = 0.01
+    eps = 0.0001
+    #eps = 0
     # this should be large to account for varying init sate
     mu_sample_size = 10**2
 
@@ -167,6 +170,7 @@ def main():
     mix_ratio = [1.0]
 
     D = np.empty((0, 5))
+    # number of episodes
     D_sample_size = 50
     for traj in get_training_data(env,
                                   pi_list=pi_behavior_list,
@@ -176,30 +180,44 @@ def main():
 
     # preprocessing D in numpy array for k
     logging.info("apprenticeship learning starts")
-    mu_mc_list = estimate_mu_mc(env, pi_exp, phi_linear, gamma, sample_size_mc)
+
+	mu_exp = AL.estimate_mu(env=env,
+							pi_eval=pi_exp,
+							mu_sample_size=sample_size_mc,
+							phi=self._phi,
+							gamma=self._gamma,
+							return_epi_len=False)
+    #mu_mc_list = estimate_mu_mc(env, pi_exp, phi_linear, gamma, sample_size_mc)
     #mu_mc_list = estimate_mu_mc(env, pi_exp, phi_rbf, gamma, sample_size_mc)
-    mu_exp = np.mean(mu_mc_list, axis=0)
+    #mu_exp = np.mean(mu_mc_list, axis=0)
 
     pi_init = pi_random
 
-    bal = BAL(env=env,
-              pi_init=pi_init,
-              D=D,
-              action_list=action_list,
-              p=p,
-              q=q,
-              phi=phi,
-              psi=psi,
-              gamma=gamma,
-              eps=eps,
-              mu_exp=mu_exp,
-              init_s_sampler=init_s_sampler,
-              mu_sample_size=mu_sample_size,
-              precision=precision,
-              use_slack=use_slack,
-              slack_penalty=slack_penalty)
+    mdp_solver = LinearQ3(env=env,
+                          estimator=Estimator(),
+                          episode_length=100,
+                          epsilon=0.0,
+                          gamma=0.99,
+                          plotting=plotting)
 
-    results = bal.run(n_trial=n_trial, n_iteration=n_iteration)
+    al = AL(env=env,
+          pi_init=pi_init,
+          action_list=action_list,
+          p=p,
+          q=q,
+          phi=phi,
+          psi=psi,
+          gamma=gamma,
+          eps=eps,
+          mu_exp=mu_exp,
+          init_s_sampler=init_s_sampler,
+          mu_sample_size=mu_sample_size,
+          precision=precision,
+          mdp_solver=mdp_solver,
+          use_slack=use_slack,
+          slack_penalty=slack_penalty)
+
+    results = al.run(n_trial=n_trial, n_iteration=n_iteration)
 
     # 5. post-process results (plotting)
     with open("data/res_{}".format(time()), "wb") as f:
