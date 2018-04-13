811be6129ad3ce4b542c4cd73826faa2ccb2e4fb
dh <dhfromkorea@gmail.com>
2018-04-13 18:30:21 -0400

    add Guassian kernel

--- util/basis.py
+++ util/basis.py
@@ -145,13 +145,90 @@ class RBFKernel2(object):
         return phi_sa
 
 
+class GaussianKernel(object):
+    """Docstring for RBFKernel. """
+
+    def __init__(self, states, n_action, p, n_component=7, include_action=False, add_bias=False):
+        """TODO: to be defined1.
+
+        assume action is discrete
+
+        Returns: n x (|A| x k) feature matrix
+
+        Parameters
+        ----------
+        env : TODO
+        n_component : TODO, optional
+        """
         # giving state action
+        self._include_action = include_action
+        self._n_component = n_component
+        self._add_bias = add_bias
+        self._n_action = n_action
+        self._p = p
+        self._phi = self.fit(states)
+
+
+    def fit(self, states):
+        # @todo: remove hard code for mountain car
+        a = np.linspace(-1.2, 0.6, self._n_component)
+        b = np.linspace(-0.07, 0.07, self._n_component)
+        mu_x, mu_y = np.meshgrid(a, b)
+        sig_x = 2*np.power((0.6+1.2)/10.,2)
+        sig_y = 2*np.power((0.07+0.07)/10.,2)
+
+        def gauss_kernel(x, s):
+            # @todo: is this gauss kernel?
+            # sig_x and sig_y different
+            # because not standardized
+            pos, speed = s
+            mu1, mu2 = x
+            return np.exp(-np.power(pos-mu1,2)/sig_x -np.power(speed-mu2,2)/sig_y)
+
+        def phi(state):
+
+            mus = np.vstack(([mu_x.T], [mu_y.T])).T
+            mus = mus.reshape(self._n_component**2, 2)
+            phi = np.apply_along_axis(gauss_kernel, 1, mus, state.flatten())
+            # intercept? I don't think we need this? or do we?
+            if self._add_bias:
+                phi = np.append(phi, [1.])
+
+            return np.array(phi)
+        return phi
+
+
+    def transform(self, s, a):
+        """
+        """
+        def helper(s, a):
+            featurized = np.zeros(self._p, dtype=np.float)
+
+            l = int(self._p*a/self._n_action)
+            r = int(self._p*(a+1)/self._n_action)
+            featurized[l:r] = self._phi(s)
+            return featurized
+
+        def helper_batch(x):
+            s, a = x[:-1], x[-1]
+            a = a.astype(np.int)
+            featurized = np.zeros(self._p, dtype=np.float)
+            l = int(self._p*a/self._n_action)
+            r = int(self._p*(a+1)/self._n_action)
+            featurized[l:r] = self._phi(s)
+            return featurized
+
         if len(s.shape) == 1:
             s = np.expand_dims(s, axis=0)
-        x = self._scaler.transform(s)
-        featurized = self._phi.transform(x)
-        return np.expand_dims(np.hstack((featurized[0], a)), axis=0)
 
+        if s.shape[0] == 1:
+            phi_sa = np.expand_dims(helper(s, a), axis=0)
+        else:
+            phi_sa = np.apply_along_axis(helper_batch, 1, np.hstack((s, a)))
+
+        assert phi_sa.shape == (s.shape[0], self._p)
+        #print("phi_sa:{}".format(phi_sa[0, :, :]))
+        return phi_sa
 
 
 def get_linear_basis(include_action=False):
